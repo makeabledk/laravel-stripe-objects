81fe40a73ae873fbdca6c7c8c184186c334d4aa3
Rasmus Christoffer Nielsen <rasmus@makeable.dk>
2017-12-10 16:02:26 +0100

    Set stripe key in service provider

--- src/Providers/StripeObjectsServiceProvider.php
+++ src/Providers/StripeObjectsServiceProvider.php
@@ -3,11 +3,14 @@
 namespace Makeable\LaravelStripeObjects\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Stripe\Stripe;
 
 class StripeObjectsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__.'/../../database/migrations/');
+
+        Stripe::setApiKey(config('services.stripe.secret'));
     }
 }
